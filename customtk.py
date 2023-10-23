2f3cc31dd95690451567dd7ebaed9fdbee3b84ff
martin gaido <martingaido00@gmail.com>
2023-10-23 18:43:13 -0300

    se agrego un calednario a el regsitrar clieten, para registrar fecha de nacimiento

--- customtk.py
+++ customtk.py
@@ -7,11 +7,12 @@ from PIL import ImageTk,Image
 import tkinter as tk
 import pygame
 import sqlite3
+from tkcalendar import Calendar
 
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 #import de utilerias de sqlite
-#from database_utils import agregar_cliente
+from database_utils import agregar_cliente
 
 conn = sqlite3.connect('BaseDatos.db') #vinculo la base de datos
 
@@ -46,8 +47,8 @@ def ventana_clientes():  #VENTANA DE CLIENTES
 
             # Cargar una imagen para volver atras
     imagen = ctk.CTkImage(light_image=Image.open("./assets/volver.png"),
-                                  dark_image=Image.open("./assets/volver.png"),
-                                  size=(30, 30))
+                                dark_image=Image.open("./assets/volver.png"),
+                                size=(30, 30))
     
 
     # Crear un botón con la imagen
@@ -85,52 +86,60 @@ def ventana_RegistrarCliente():
     
 
     imagen = ctk.CTkImage(light_image=Image.open("./assets/volver.png"),
-                                  dark_image=Image.open("./assets/volver.png"),
-                                  size=(30, 30))
+                                dark_image=Image.open("./assets/volver.png"),
+                                size=(30, 30))
     
 
     # Crear un botón con la imagen
     boton_con_imagen = ctk.CTkButton(RegistrarCielte, image=imagen,text="Volver", command=lambda: (cambiarVentana(RegistrarCielte, ventana_main)))
     boton_con_imagen.place(relx=0.03, rely=0.03)
     #FrameClientes
-    frame = customtkinter.CTkFrame(master=RegistrarCielte, width=320, height=500, corner_radius=15, border_color="black",border_width=12)
+    frame = customtkinter.CTkFrame(master=RegistrarCielte, width=320, height=600, corner_radius=15, border_color="black",border_width=12)
     frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
     #LABEL TITULO
     label_titulo = customtkinter.CTkLabel(master=frame, text="Registrar clientes", font=('Century Gothic',20))
-    label_titulo.place(relx=0.26, rely=0.05)
+    label_titulo.place(relx=0.26, rely=0.03)
 
     #Entradas
     label_nombre = customtkinter.CTkLabel(master=frame, text="Nombre", font=('Century Gothic',15))
-    label_nombre.place(relx=0.32, rely=0.15)
+    label_nombre.place(relx=0.32, rely=0.1)
     entry_nombre = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_nombre.place(relx=0.5, rely=0.22, anchor=tk.CENTER)
+    entry_nombre.place(relx=0.5, rely=0.16, anchor=tk.CENTER)
 
     label_apellido = customtkinter.CTkLabel(master=frame, text="Apellido", font=('Century Gothic',15))
-    label_apellido.place(relx=0.32, rely=0.27)
+    label_apellido.place(relx=0.32, rely=0.2)
     entry_apellido = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_apellido.place(relx=0.5, rely=0.34, anchor=tk.CENTER)
+    entry_apellido.place(relx=0.5, rely=0.26, anchor=tk.CENTER)
 
     label_documento = customtkinter.CTkLabel(master=frame, text="Documento", font=('Century Gothic',15))
-    label_documento.place(relx=0.32, rely=0.39)
+    label_documento.place(relx=0.32, rely=0.3)
     entry_documento = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_documento.place(relx=0.5, rely=0.46, anchor=tk.CENTER)
+    entry_documento.place(relx=0.5, rely=0.36, anchor=tk.CENTER)
 
     label_correo = customtkinter.CTkLabel(master=frame, text="Correo", font=('Century Gothic',15))
-    label_correo.place(relx=0.32, rely=0.51)
+    label_correo.place(relx=0.32, rely=0.4)
     entry_correo = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_correo.place(relx=0.5, rely=0.58, anchor=tk.CENTER)
+    entry_correo.place(relx=0.5, rely=0.46, anchor=tk.CENTER)
 
     label_telefono = customtkinter.CTkLabel(master=frame, text="Telefono", font=('Century Gothic',15))
-    label_telefono.place(relx=0.32, rely=0.63)
+    label_telefono.place(relx=0.32, rely=0.5)
     entry_telefono = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_telefono.place(relx=0.5, rely=0.70, anchor=tk.CENTER)
+    entry_telefono.place(relx=0.5, rely=0.56, anchor=tk.CENTER)
+
 
 
     label_fechaNacimiento = customtkinter.CTkLabel(master=frame, text="Fecha de nacimiento", font=('Century Gothic',15)) # se ingresa ejemplo "año-mes-dia"
-    label_fechaNacimiento.place(relx=0.32, rely=0.75)
-    entry_fechaNacimiento = customtkinter.CTkEntry(master=frame, width=120, height=25, corner_radius=10)
-    entry_fechaNacimiento.place(relx=0.5, rely=0.82, anchor=tk.CENTER)
+    label_fechaNacimiento.place(relx=0.32, rely=0.58)
+
+    frameCalendario = customtkinter.CTkFrame(master=frame, width=300, height=300)
+    frameCalendario.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
+    
+    # Crear un objeto Calendar
+    cal = Calendar(frameCalendario, selectmode="day", year=2023, month=10, day=23)
+
+    # Colocar los widgets en la ventana
+    cal.pack(pady=10)
 
     
     #boton confirmar
@@ -138,7 +147,7 @@ def ventana_RegistrarCliente():
         master=frame,
         width=220,
         text="Confirmar",
-        command=lambda: agregar_cliente([entry_nombre.get(),entry_apellido.get(), entry_correo.get(), entry_documento.get(), entry_fechaNacimiento.get(), entry_telefono.get()]),
+        command=lambda: agregar_cliente([entry_nombre.get(),entry_apellido.get(), entry_correo.get(), entry_documento.get(), cal.get_date(), entry_telefono.get()]),
         corner_radius=6
     )
     button_confirmar.place(relx=0.18, rely=0.90)
@@ -280,8 +289,8 @@ def ventana_AcutalizarClientes():
     actualizarClientes.title("Actualizar precios")
 
     imagen = customtkinter.CTkImage(light_image=Image.open("./assets/volver.png"),
-                                  dark_image=Image.open("./assets/volver.png"),
-                                  size=(30, 30))
+                                dark_image=Image.open("./assets/volver.png"),
+                                size=(30, 30))
     
 
     # Crear un botón con la imagen
