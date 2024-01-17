d51f59dccc653a1a9ff76da99e3a162709d8da85
martin gaido <martingaido00@gmail.com>
2024-01-17 12:27:01 -0300

    a informes se agrega la columna estado

--- informes.py
+++ informes.py
@@ -1,3 +1,4 @@
+"""
 import sqlite3
 import pandas as pd
 import subprocess
@@ -46,4 +47,51 @@ def generar_completo():
     conn.close()
 
     subprocess.Popen(["start", "excel", "datos_clientes.xlsx"], shell=True)
+"""
+import sqlite3
+import pandas as pd
+import subprocess
+from datetime import datetime
+
+def generar_completo():
+    # Conectar a la base de datos SQLite
+    conn = sqlite3.connect("BaseDatos.db")
+
+    # Consulta SQL para obtener solo las columnas necesarias de la tabla "Clientes"
+    consulta_clientes = "SELECT id, Apellido, Nombre, Documento, Telefono, Correo, Fecha_Nacimiento FROM Clientes;"
+
+    # Consulta SQL para obtener solo las columnas necesarias de la tabla "Cuotas"
+    consulta_cuotas = "SELECT Cuotas.id_cliente, Cuotas.Haber, Cuotas.[PLAN], Cuotas.Fecha, Cuotas.Vencimiento FROM Cuotas;"
+
+    # Leer los datos en DataFrames de pandas
+    df_clientes = pd.read_sql_query(consulta_clientes, conn)
+    df_cuotas = pd.read_sql_query(consulta_cuotas, conn)
+
+    # Realizar un join entre las tablas usando la columna 'id_cliente'
+    df_completo = pd.merge(df_cuotas, df_clientes, left_on='id_cliente', right_on='id', how='left')
+
+    # Convertir la columna 'Vencimiento' a objetos datetime
+    df_completo['Vencimiento'] = pd.to_datetime(df_completo['Vencimiento'], errors='coerce')
+
+    # Agregar una nueva columna 'Estado' basada en la comparación entre fecha actual y fecha de vencimiento
+    fecha_actual = datetime.now()
+    df_completo['Estado'] = df_completo['Vencimiento'].apply(lambda x: 'Activo' if pd.notnull(x) and x > fecha_actual else 'Vencido')
+
+    # Formatear la columna 'Vencimiento' antes de guardarla en el archivo Excel
+    df_completo['Vencimiento_Formateada'] = df_completo['Vencimiento'].dt.strftime('%d/%m/%Y')
+
+    # Seleccionar solo las columnas necesarias, incluyendo la nueva columna 'Estado'
+    columnas_seleccionadas = ['Apellido', 'Nombre', 'Documento', 'Telefono', 'Correo', 'Fecha_Nacimiento', 'PLAN', 'Haber', 'Fecha', 'Vencimiento_Formateada', 'Estado']
+    df_resultado = df_completo[columnas_seleccionadas]
+
+    # Guardar el DataFrame resultante en un archivo Excel
+    df_resultado.to_excel("datos_clientes.xlsx", index=False)
+
+    # Cerrar la conexión a la base de datos
+    conn.close()
+
+    subprocess.Popen(["start", "excel", "datos_clientes.xlsx"], shell=True)
+
+# Llamada a la función
+generar_completo()
 
