8ecff788299fca4f5fa32fb40d3e181e15ee0345
martin gaido <martingaido00@gmail.com>
2024-02-11 15:38:49 -0300

    se termino la funcion de notificar clientes

--- logicaBotones.py
+++ logicaBotones.py
@@ -1011,8 +1011,40 @@ def asistencias(self):
 
 
 
+def obtener_notificaciones_recientes():
+    # Conexión a la base de datos SQLite
+    conn = sqlite3.connect('BaseDatos.db')
+    cursor = conn.cursor()
+
+    # Fecha actual menos 5 días
+    fecha_limite = datetime.now() - timedelta(days=5)
+
+    # Consulta SQL para obtener las notificaciones recientes
+    consulta = """
+    SELECT * FROM Notificacion
+    WHERE Fecha >= ?
+    """
+    cursor.execute(consulta, (fecha_limite,))
+
+    # Obtener los resultados
+    filas = cursor.fetchall()
+
+    # Cerrar la conexión y retornar las filas obtenidas
+    conn.close()
+    return filas
+
+def valor_en_columna(valor, lista_filas):
+    # Iterar sobre cada fila en la lista
+    for fila in lista_filas:
+        # Iterar sobre cada elemento de la fila
+        for elemento in fila:
+            # Verificar si el valor está presente en el elemento actual
+            if valor == elemento:
+                return True
+    # Si no se encuentra en ninguna fila, retornar False
+    return False
+
 def conocerClientesPorVencer():
-    
     data = []
     info_cliente = []
 
@@ -1038,6 +1070,8 @@ def conocerClientesPorVencer():
     # Lista para almacenar los datos de los clientes cuyas cuotas se vencen en 5 o menos días
     clientes_vencimiento_cercano = []
 
+    clientes_notificados_recientes = obtener_notificaciones_recientes()
+
     # Iterar sobre los resultados y realizar el filtrado fuera de la consulta
     for fila in resultados:
         # Convertir la fecha de vencimiento de la cuota a un objeto datetime
@@ -1047,34 +1081,51 @@ def conocerClientesPorVencer():
         # Calcular la diferencia de días entre la fecha actual y la fecha de vencimiento de la cuota
         diferencia_dias = (fecha_vencimiento_cuota - fecha_actual).days
         # Si la diferencia es de 5 días o menos, añadir los datos del cliente y la cuota al array
-        print(diferencia_dias)
         if 0 < diferencia_dias < 5:
             clientes_vencimiento_cercano.append(fila)
 
     for cliente_cuota_info in clientes_vencimiento_cercano:
-        # Extraer la información necesaria de la tupla cliente_cuota_info
+        if not valor_en_columna(cliente_cuota_info[0], clientes_notificados_recientes):
+            # Extraer la información necesaria de la tupla cliente_cuota_info
 
-        numero_telefono = cliente_cuota_info[6]  # Número de teléfono del cliente
-        fecha_vencimiento_cuota = cliente_cuota_info[-3]  # Fecha de vencimiento de la cuota
+            numero_telefono = cliente_cuota_info[6]  # Número de teléfono del cliente
+            fecha_vencimiento_cuota = cliente_cuota_info[-3]  # Fecha de vencimiento de la cuota
 
-        # Construir el mensaje para el cliente
-        mensaje = f"Su cuota vencerá en los próximos días, la fecha de vencimiento es {fecha_vencimiento_cuota}"
+            fecha_vencimiento_datetime = datetime.strptime(str(fecha_vencimiento_cuota), "%d/%m/%Y")
 
-        # Crear el diccionario con la información del cliente y su cuota
-        cliente = {
-            "numero": str(numero_telefono),  # Convertir a cadena de texto si es necesario
-            "mensaje": mensaje
-        }
+            print((fecha_vencimiento_datetime - fecha_actual).days + 1)
 
-        # Agregar el diccionario a la lista data
-        data.append(cliente)
-        info_cliente.append(cliente_cuota_info)
+            # Construir el mensaje para el cliente
+            mensaje = f"Hola *{cliente_cuota_info[2]}*! Salud integral le recuerda que su cuota de *{cliente_cuota_info[10]}* esta a {(fecha_vencimiento_datetime - fecha_actual).days + 1} de vencer. Muchas Gracias!"
+            print(mensaje)
+            # Crear el diccionario con la información del cliente y su cuota
+            cliente = {
+                "numero": str(numero_telefono),  # Convertir a cadena de texto si es necesario
+                "mensaje": mensaje
+            }
+
+            # Agregar el diccionario a la lista data
+            data.append(cliente)
+            info_cliente.append(cliente_cuota_info)
 
     # Cerrar la conexión
     conn.close()
     
     return (data, info_cliente)
 
+
+
+def guardar_notificacion(tipo, id_cliente, mensaje):
+    # Conexión a la base de datos SQLite
+    conn = sqlite3.connect('BaseDatos.db')
+    cursor = conn.cursor()
+    # Fecha actual como un objeto datetime
+    fecha_actual = datetime.now()
+    # Insertar los datos en la tabla Notificacion
+    cursor.execute("INSERT INTO Notificacion (Tipo, id_cliente, Mensaje, Fecha) VALUES (?, ?, ?, ?)", (tipo, id_cliente, mensaje, fecha_actual))
+    # Confirmar la transacción y cerrar la conexión
+    conn.commit()
+    conn.close()
 
 
 def whatsapp_api(self):
@@ -1085,15 +1136,20 @@ def whatsapp_api(self):
         print("button pressed")
         response = requests.post(url, json=data)
         if response.status_code == 200:
-            print("Solicitud POST exitosa")
             print("Respuesta del servidor:", response.json())
+            for i in range(len(data)):
+                print("Enviando notificación para:", data[i]["mensaje"])
+                guardar_notificacion("whatsapp_ProxVencimiento", info_cliente[i][0], data[i]["mensaje"])
+                #aca ejecutar limpiar etiquetanotificacion
+            for child in notificar.winfo_children():
+                child.destroy()
         else:
             print("Error al enviar la solicitud POST:", response.status_code)
 
     notificar = customtkinter.CTkScrollableFrame(self, label_text="Notificar")
     notificar.pack(pady=10)
     for i in info_cliente:
-        etiqueta_notificacion = customtkinter.CTkLabel(notificar, text=f"{i[1]} {i[2]} - Vence pronto")
+        etiqueta_notificacion = customtkinter.CTkLabel(notificar, text=f"{i[1]} {i[2]} - Vence pronto", font=("Arial", 12))
         etiqueta_notificacion.pack(fill="x", padx=0, pady=5)
     button = customtkinter.CTkButton(self, text="Enviar Mensaje", command=button_event)
     button.pack(pady=10)
